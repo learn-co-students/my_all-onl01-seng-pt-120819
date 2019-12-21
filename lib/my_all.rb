4161fa090455445f7ce8fa08dbebd1168b5162fb
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-21 02:28:44 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
+[1,2,3].yield array= [1,2,3]
+end
+def my_all?(collection)
+if i<3
+  puts "false"
+end
 
+def my_all?(collection)
+if i<4
+  puts "true"
 end
